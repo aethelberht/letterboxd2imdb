c5512a0581570229a1d5994b427f41c139c1f37d
aethelberht <aethelberht@versiontwo.org>
2014-10-11 20:20:50 -0400

    Added letterboxd lookup

--- lbd2imdb.py
+++ lbd2imdb.py
@@ -6,55 +6,66 @@
 # Does not fill in release date and title type fields as omdbapi does 
 # not give complete information on these
 #
-# 10/02/2014
+# 10/11/2014
 
 import json
 import requests
 import csv
-import datetime
+from datetime import date
+from bs4 import BeautifulSoup
 
-#showtype = {"Short":"Short Film", "episode":"TV Episode", "series":"TV Series"}
+LETTERBOXD_FILE = 'input.csv'
+IMDBSTYLE_FILE = 'output.csv'
 
-with open('fyrabanks.csv', newline='') as csvfile:
-    with open('fyrabanks-imdb2.csv', 'w', newline='') as outfile:
-        filmout = csv.writer(outfile, quoting=csv.QUOTE_ALL)
-        filmout.writerow(["position","const","created","modified","description","Title","Title type","Directors","You rated","IMDb Rating","Runtime (mins)","Year","Genres","Num. Votes","Release Date (month/day/year)","URL"])
-        filmread = csv.reader(csvfile, delimiter=',', quotechar='"')
-        next(filmread)
-        idx = 0
-        for row in filmread:
-            title = row[1]
-            year = row[2]
-            if row[5] != "":
-                query = 'http://www.omdbapi.com/?i=' + row[5]
-            else:
-                query = 'http://www.omdbapi.com/?t=' + title + '&y=' + year
-            r = requests.get(query).json()
-            #print(r.json()['Response'])
-            rating = str(int(float(row[4]) * 2))
-            if r['Response'] == 'True':
-                idx += 1
-                rd = row[0]
-                rd = rd.split('/')
-                rd = datetime.date(int(rd[2]),int(rd[0]),int(rd[1])).strftime('%a %b %d 00:00:00 %Y')
-                url = 'http://www.imdb.com/title/' + r['imdbID'] + '/'
-                if r['imdbRating'] == 'N/A':
-                    irating = ''
-                    votes = ''
-                else:
-                    irating = r['imdbRating']
-                    votes = r['imdbVotes']
-                    
-                if r['Runtime'] == 'N/A':
-                    runtime = ''
-                else:
-                    runtime = r['Runtime'][:-4]
+with open(LETTERBOXD_FILE, newline='') as csvfile, open(IMDBSTYLE_FILE, 'w', newline='') as outfile:
+    filmout = csv.writer(outfile, quoting=csv.QUOTE_ALL)
+    filmout.writerow(["position","const","created","modified","description","Title","Title type","Directors","You rated","IMDb Rating","Runtime (mins)","Year","Genres","Num. Votes","Release Date (month/day/year)","URL"])
+    filmread = csv.reader(csvfile, delimiter=',', quotechar='"')
+    next(filmread)
+    idx = 0
 
-                filmout.writerow([str(idx), r['imdbID'], rd, "", "", r['Title'], "", "", rating, irating, runtime, r['Year'], "", votes, "", url])
-                #print(r['imdbID'])
-            else:
-                print(title + ' ' + str(year))
+    for row in filmread:
+        if idx % 20 == 0:
+            print(str(idx) + " films processed.")
 
+        # Scrapes letterboxd for imdb link
+        r = requests.get(row[3])
+        r_soup = BeautifulSoup(r.text)
+        s = r_soup.find("p", "text-link").find('a')['href']
+        filmid = s.split('/')[4]
+        # Grabs json data from imdb-external api
+        r = requests.get('http://www.omdbapi.com/?i=' + filmid).json()
+        
+        rating = str(int(float(row[4]) * 2))
+        #if r['Response'] == 'True':
+        idx += 1
+        rd = row[0].split('-')
+        rd = date(int(rd[0]),int(rd[1]),int(rd[2])).strftime('%a %b %d 00:00:00 %Y')
+        url = 'http://www.imdb.com/title/' + r['imdbID'] + '/'
+
+        if r['imdbRating'] == 'N/A':
+            imdbrating, votes = ('','')
+        else:
+            imdbrating, votes = (r['imdbRating'], r['imdbVotes'])
+
+        if r['Runtime'] == 'N/A':
+            runtime = ''
+        else:
+            runtime = r['Runtime'][:-4]
+
+        # Extract up to two directors
+        director = r['Director'].split(', ')
+        director = ', '.join(director[:2])
+
+        genres = r['Genre'].lower()
+        genres = genres.replace('sci-fi', 'sci_fi')
+
+        filmout.writerow([str(idx), r['imdbID'], rd, "", "", r['Title'], "", director, rating, imdbrating, runtime, r['Year'], genres, votes, "", url])
+            
+
+# Director: Take only two directors
+
+# omdb json format:
 
 # {'imdbID': 'tt0040897',
 #  'Response': 'True',
