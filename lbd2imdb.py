887f530acaf3a2eae48e50d12e55bb4a8488cec6
Aethelberht <aethelberht@users.noreply.github.com>
2020-10-17 12:06:29 -0700

    Begin update to latest imdb/letterboxd format

--- lbd2imdb.py
+++ lbd2imdb.py
@@ -1,9 +1,9 @@
-#! python3
+#!/usr/bin/env python3
 #
-# lbd2imdb.py: Takes exported csv from letterboxd and converts it to 
+# lbd2imdb.py: Takes exported csv from letterboxd and converts it to
 # imdb format so it may then be imported to other services
 #
-# Does not fill in release date and title type fields as omdbapi does 
+# Does not fill in release date and title type fields as omdbapi does
 # not give complete information on these
 #
 # 10/11/2014
@@ -13,32 +13,46 @@ import requests
 import csv
 from datetime import date
 from bs4 import BeautifulSoup
+from imdb import IMDb
 
 LETTERBOXD_FILE = 'input.csv'
-IMDBSTYLE_FILE = 'output.csv'
+IMDBSTYLE_FILE = 'output234.csv'
 
-with open(LETTERBOXD_FILE, newline='') as csvfile, open(IMDBSTYLE_FILE, 'w', newline='') as outfile:
+imdb_access = IMDb()
+
+with open(LETTERBOXD_FILE, newline='') as letterboxd, open(IMDBSTYLE_FILE, 'w', newline='') as outfile:
+    head = ["Const", "Your Rating", "Date Rated", "Title", "URL", "Title Type", "IMDb Rating", "Runtime (mins)", "Year", "Genres", "Num Votes", "Release Date", "Directors"]
     filmout = csv.writer(outfile, quoting=csv.QUOTE_ALL)
-    filmout.writerow(["position","const","created","modified","description","Title","Title type","Directors","You rated","IMDb Rating","Runtime (mins)","Year","Genres","Num. Votes","Release Date (month/day/year)","URL"])
-    filmread = csv.reader(csvfile, delimiter=',', quotechar='"')
-    next(filmread)
-    idx = 0
+    filmout.writerow(head)
 
-    for row in filmread:
-        if idx % 20 == 0:
+    filmread = csv.reader(letterboxd, delimiter=',', quotechar='"')
+    # Expected letterboxd format:
+    # Date,Name,Year,Letterboxd URI,Rating
+    next(filmread)
+    for idx, row in enumerate(filmread):
+        if idx > 0 and idx % 20 == 0:
             print(str(idx) + " films processed.")
 
         # Scrapes letterboxd for imdb link
         r = requests.get(row[3])
-        r_soup = BeautifulSoup(r.text)
-        s = r_soup.find("p", "text-link").find('a')['href']
-        filmid = s.split('/')[4]
-        # Grabs json data from imdb-external api
-        r = requests.get('http://www.omdbapi.com/?i=' + filmid).json()
-        
+        r_soup = BeautifulSoup(r.text, features="lxml")
+        s = r_soup.find(attrs={"data-track-action": "IMDb"})['href']
+        filmid = s.split('/')[4][2:]
+        print(filmid)
+        imdb_film = imdb_access.get_movie(filmid)
+        print(f"title: {imdb_film['title']}")
+        print(f"imdb rating: {imdb_film['rating']} ({imdb_film['votes']} votes)")
+        print(f"directors: {[d['name'] for d in imdb_film['directors']]}")
+        print(f"your rating: {int(float(row[4]) * 2)}")
+        print(f"date rated: {row[0]}")
+        print(f"url: http://www.imdb.com/title/tt{filmid}/")
+
+        if idx == 3:
+            quit()
+        else:
+            continue
+
         rating = str(int(float(row[4]) * 2))
-        #if r['Response'] == 'True':
-        idx += 1
         rd = row[0].split('-')
         rd = date(int(rd[0]),int(rd[1]),int(rd[2])).strftime('%a %b %d 00:00:00 %Y')
         url = 'http://www.imdb.com/title/' + r['imdbID'] + '/'
@@ -61,7 +75,9 @@ with open(LETTERBOXD_FILE, newline='') as csvfile, open(IMDBSTYLE_FILE, 'w', new
         genres = genres.replace('sci-fi', 'sci_fi')
 
         filmout.writerow([str(idx), r['imdbID'], rd, "", "", r['Title'], "", director, rating, imdbrating, runtime, r['Year'], genres, votes, "", url])
-            
+
+if __name__ == '__main__':
+    main()
 
 # Director: Take only two directors
 
