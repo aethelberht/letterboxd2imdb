2a8a48dc245f4b6d83540016a81350fa961beab1
Aethelberht <aethelberht@users.noreply.github.com>
2020-10-17 17:40:51 -0700

    Finish transition to imdbpy

--- lbd2imdb.py
+++ lbd2imdb.py
@@ -1,105 +1,115 @@
 #!/usr/bin/env python3
 #
-# lbd2imdb.py: Takes exported csv from letterboxd and converts it to
+# lbd2imdb.py: Takes exported ratings.csv from letterboxd and converts it to
 # imdb format so it may then be imported to other services
 #
-# Does not fill in release date and title type fields as omdbapi does
-# not give complete information on these
-#
-# 10/11/2014
+# Does not fill in release date.
 
-import json
+from bs4 import BeautifulSoup
 import requests
 import csv
-from datetime import date
-from bs4 import BeautifulSoup
+# from datetime import date
 from imdb import IMDb
 
-LETTERBOXD_FILE = 'input.csv'
-IMDBSTYLE_FILE = 'output234.csv'
+LETTERBOXD_FILE = 'ratings.csv'
+IMDBSTYLE_FILE = 'ratings-imdbfy.csv'
 
 imdb_access = IMDb()
 
-with open(LETTERBOXD_FILE, newline='') as letterboxd, open(IMDBSTYLE_FILE, 'w', newline='') as outfile:
-    head = ["Const", "Your Rating", "Date Rated", "Title", "URL", "Title Type", "IMDb Rating", "Runtime (mins)", "Year", "Genres", "Num Votes", "Release Date", "Directors"]
-    filmout = csv.writer(outfile, quoting=csv.QUOTE_ALL)
-    filmout.writerow(head)
+def get_imdb_movie(title, year):
+    results = [r for r in imdb_access.search_movie(title) if r['title'] == title and 'year' in r and \
+        r['year'] == int(year) and r['kind'] not in ['video game', 'episode']]
+    if len(results) == 1:
+        imdb_access.update(results[0], info=['main'])
+        return results[0]
+    elif len(results) == 0:
+        print(f"No results found for {title} ({year})")
+    elif len(results) > 1:
+        print(f"Multiple results found for {title} ({year})")
+    return None
 
-    filmread = csv.reader(letterboxd, delimiter=',', quotechar='"')
-    # Expected letterboxd format:
-    # Date,Name,Year,Letterboxd URI,Rating
-    next(filmread)
-    for idx, row in enumerate(filmread):
-        if idx > 0 and idx % 20 == 0:
-            print(str(idx) + " films processed.")
 
-        # Scrapes letterboxd for imdb link
-        r = requests.get(row[3])
-        r_soup = BeautifulSoup(r.text, features="lxml")
-        s = r_soup.find(attrs={"data-track-action": "IMDb"})['href']
-        filmid = s.split('/')[4][2:]
-        print(filmid)
-        imdb_film = imdb_access.get_movie(filmid)
-        print(f"title: {imdb_film['title']}")
-        print(f"imdb rating: {imdb_film['rating']} ({imdb_film['votes']} votes)")
-        print(f"directors: {[d['name'] for d in imdb_film['directors']]}")
-        print(f"your rating: {int(float(row[4]) * 2)}")
-        print(f"date rated: {row[0]}")
-        print(f"url: http://www.imdb.com/title/tt{filmid}/")
+def get_movie_via_letterboxd(url):
+    # Scrapes letterboxd for imdb link
+    lbxd_data = requests.get(url)
+    lbxd_soup = BeautifulSoup(lbxd_data.text, features="lxml")
+    imdb_url = lbxd_soup.find(attrs={"data-track-action": "IMDb"})['href']
+    return imdb_access.get_movie(imdb_url.split('/')[4][2:])
 
-        if idx == 3:
-            quit()
-        else:
-            continue
 
-        rating = str(int(float(row[4]) * 2))
-        rd = row[0].split('-')
-        rd = date(int(rd[0]),int(rd[1]),int(rd[2])).strftime('%a %b %d 00:00:00 %Y')
-        url = 'http://www.imdb.com/title/' + r['imdbID'] + '/'
+def translate_type(kind):
+    # Convert kind returned by imdbpy to Title Type
+    # FIXME: tvSpecial is unsupported
+    types = {
+        "movie": "movie",
+        "short": "short",
+        "video movie": "video",
+        "tv movie": "tvMovie",
+        "tv mini series": "tvMiniSeries",
+        "tv short": "tvShort",
+    }
+    return types[kind]
 
-        if r['imdbRating'] == 'N/A':
-            imdbrating, votes = ('','')
-        else:
-            imdbrating, votes = (r['imdbRating'], r['imdbVotes'])
 
-        if r['Runtime'] == 'N/A':
-            runtime = ''
-        else:
-            runtime = r['Runtime'][:-4]
+def main():
+    with open(LETTERBOXD_FILE, newline='') as letterboxd:
+        # Expected letterboxd format:
+        # Date,Name,Year,Letterboxd URI,Rating
+        input_csv = csv.reader(letterboxd, delimiter=',', quotechar='"')
+        next(input_csv)
+        letterboxd_films = []
+        for row in input_csv:
+            letterboxd_films.append(row)
 
-        # Extract up to two directors
-        director = r['Director'].split(', ')
-        director = ', '.join(director[:2])
+    with open(IMDBSTYLE_FILE, 'w', newline='') as outfile:
+        head = ["Const", "Your Rating", "Date Rated", "Title", "URL", "Title Type", "IMDb Rating", \
+            "Runtime (mins)", "Year", "Genres", "Num Votes", "Release Date", "Directors"]
+        filmout = csv.writer(outfile, quoting=csv.QUOTE_MINIMAL)
+        filmout.writerow(head)
 
-        genres = r['Genre'].lower()
-        genres = genres.replace('sci-fi', 'sci_fi')
+        for idx, row in enumerate(letterboxd_films):
+            if idx > 0 and idx % 20 == 0:
+                print(str(idx) + " films processed.")
 
-        filmout.writerow([str(idx), r['imdbID'], rd, "", "", r['Title'], "", director, rating, imdbrating, runtime, r['Year'], genres, votes, "", url])
+            title, year = row[1], row[2]
+            film = get_imdb_movie(title, year)
+            if not film:
+                print("Falling back to letterboxd scrape.")
+                film = get_movie_via_letterboxd(row[3])
+            print(f"found: {film['title']}, {film['year']}")
+
+            # print(f"title: {film['title']}")
+            # print(f"imdb rating: {film['rating']} ({film['votes']} votes)")
+            # print(f"directors: {', '.join([d['name'] for d in film['directors']])}")
+            # print(f"your rating: {int(float(row[4]) * 2)}")
+            # print(f"date rated: {row[0]}")
+            # print(f"url: http://www.imdb.com/title/tt{film.movieID}/")
+            # print(f"genres: {', '.join(film['genres'])}")
+            # print(f"type: {film['kind']} -> {translate_type(film['kind'])}")
+            if len(film['runtime']) > 1:
+                raise ValueError("Multiple runtimes found.")
+            else:
+                runtime = int(film['runtime'][0])
+
+            if 'directors' in film:
+                directors = [d['name'] for d in film['directors']]
+            else:
+                directors = []
+
+            filmout.writerow([
+                f"tt{film.movieID}",
+                int(float(row[4]) * 2),
+                row[0],
+                film['title'],
+                f"http://www.imdb.com/title/tt{film.movieID}/",
+                translate_type(film['kind']),
+                runtime,
+                film['year'],
+                ', '.join(film['genres']),
+                film['votes'],
+                '',
+                ', '.join([d['name'] for d in film['directors']])
+            ])
 
 if __name__ == '__main__':
     main()
-
-# Director: Take only two directors
-
-# omdb json format:
-
-# {'imdbID': 'tt0040897',
-#  'Response': 'True',
-#  'Metascore': 'N/A',
-#  'Writer': 'John Huston (screenplay), B. Traven (based on the novel by)',
-#  'Genre': 'Action, Adventure, Drama',
-#  'Type': 'movie',
-#  'Runtime': '126 min',
-#  'Country': 'USA',
-#  'Rated': 'TV-PG',
-#  'Director': 'John Huston',
-#  'Released': '24 Jan 1948',
-#  'Language': 'English, Spanish',
-#  'Title': 'The Treasure of the Sierra Madre',
-#  'Poster': 'http://ia.media-imdb.com/images/M/MV5BMTQ4MzUzOTYwOV5BMl5BanBnXkFtZTgwNDA4MzgyMjE@._V1_SX300.jpg',
-#  'imdbRating': '8.4',
-#  'Year': '1948',
-#  'Awards': 'Won 3 Oscars. Another 18 wins & 4 nominations.',
-#  'Plot': 'Fred Dobbs and Bob Curtin, two Americans searching for work in Mexico, convince an old prospector to help them mine for gold in the Sierra Madre Mountains.',
-#  'imdbVotes': '63,325',
-#  'Actors': 'Humphrey Bogart, Walter Huston, Tim Holt, Bruce Bennett'}
